4603a6ce791672c68159344d08371396e3f15c59
RyanJones999 <78021617+RyanJones999@users.noreply.github.com>
2023-11-08 15:07:39 +0000

    switched javascript runtime to deno

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,5 @@
-import express from "express";
-import cors from "cors";
+import express from "npm:express";
+import cors from "npm:cors";
 import * as handlers from "./handlers.js";
 
 // Set the port
