bf7581e0b831202334ef2284db0c6dbb3c85bd0b
RyanJones999 <78021617+RyanJones999@users.noreply.github.com>
2023-11-08 10:29:00 +0000

    adjusted the global decleration

--- server/src/global.js
+++ server/src/global.js
@@ -1,4 +1,4 @@
-export const items = []; //removed type annotations as i am no longer using typescript
+globalThis.items = []; //removed type annotations as i am no longer using typescript
 
 //After doing some research it is recommended to avoid using the type 'any' to maintain safety and ensure code clarity.
 //Instead I can declare a set of types including a nested list for keywords, which only needs to contain strings
